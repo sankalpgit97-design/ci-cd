ac860261a3ce41f073b94c5dd294538abfee580a
sankalpgit97-design <sankalpgit97@gmail.com>
2026-09-01 14:25:28 +0530

    git commit 1.3

--- tests/validate.spec.js
+++ tests/validate.spec.js
@@ -2,12 +2,12 @@ const { test, expect } = require('@playwright/test');
 
 test('Validate dashboard title and execute logout' ,async ({page}) => {
 
-await page.goto(loginDaata.dasboardUrl);
+await page.goto(loginData.expectedUrl);
 
 await expect(page).toHaveTitle('Logged In Successfully | Practice Test Automation');
 const logoutButton = page.locator('text=Log out');
 await expect(logoutButton).toBeVisible();
 await logoutButton.click();
 
-await expect(page).toHaveURL(loginData.loginUrl);
+await expect(page).toHaveURL(loginData.expectedUrl);
 });

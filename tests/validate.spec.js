64ee6c02c3f4240873be71bb7ff3bcdc59d2df9e
sankalpgit97-design <sankalpgit97@gmail.com>
2026-09-01 14:37:55 +0530

    git commit 1.7

--- tests/validate.spec.js
+++ tests/validate.spec.js
@@ -4,12 +4,12 @@ const dataPath = path.join(__dirname, '../data/loginData.json');
 
 test('Validate dashboard title and execute logout' ,async ({page}) => {
 
-await page.goto(loginData.expectedUrl);
+await page.goto(testData.expectedUrl);
 
 await expect(page).toHaveTitle('Logged In Successfully | Practice Test Automation');
 const logoutButton = page.locator('text=Log out');
 await expect(logoutButton).toBeVisible();
 await logoutButton.click();
 
-await expect(page).toHaveURL(loginData.expectedUrl);
+await expect(page).toHaveURL(testData.expectedUrl);
 });

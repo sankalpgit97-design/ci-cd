a6bd53e55cc8fee2496a7d383bc37033254648a6
sankalpgit97-design <sankalpgit97@gmail.com>
2026-09-01 14:41:22 +0530

    git commit 1.8

--- tests/validate.spec.js
+++ tests/validate.spec.js
@@ -1,6 +1,9 @@
 const { test, expect } = require('@playwright/test');
+const fs = require('fs');
 const path = require('path');
+
 const dataPath = path.join(__dirname, '../data/loginData.json');
+const testData = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
 
 test('Validate dashboard title and execute logout' ,async ({page}) => {
 
